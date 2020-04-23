bca2e5f6593725e9e89a2ba697d70fc662e2de3e
Gabriel <gabriel.bastos13@hotmail.com>
2020-04-23 18:52:55 -0300

    first add

---
+++ src/components/Cart/CartColumns.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function CartColumns() {
+    return (
+        <div className='container-fluid text-center d-none d-lg-block'>
+        <div className='row'>
+            <div className='col-10 mx-auto col-lg-2'>
+            <p className='text-uppercase'>Produto</p>
+            </div>
+            <div className='col-10 mx-auto col-lg-2'>
+            <p className='text-uppercase'>Nome do Produto</p>
+            </div>
+            <div className='col-10 mx-auto col-lg-2'>
+            <p className='text-uppercase'>Preço</p>
+            </div>
+            <div className='col-10 mx-auto col-lg-2'>
+            <p className='text-uppercase'>QTD</p>
+            </div>
+            <div className='col-10 mx-auto col-lg-2'>
+            <p className='text-uppercase'>Remover</p>
+            </div>
+            <div className='col-10 mx-auto col-lg-2'>
+            <p className='text-uppercase'>Total</p>
+            </div>
+
+        </div>            
+        </div>
+    )
+}
